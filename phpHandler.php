7a5d4b8b1f363a32cbbc68c2f0f327cc70a24323
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-18 20:25:35 +0300

    Method to insert user data to db

--- phpHandler.php
+++ phpHandler.php
@@ -4,7 +4,16 @@ class phpHandler{
 
    // Constructor 
 public function __construct($pdo){
-    $this->db = $pdo
+    $this->db = $pdo;
+}
+//insert user data
+public function insertData($name, $email, $password){
+    $hashedPassword= password_hash($password,PASSWORD_BCRYPT);
+    $sql = "INSERT INTO users(name, email, password) VALUES(:name, :email, :password)";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $hashedPassword]);
+    return true;
+
 }
 
 }
