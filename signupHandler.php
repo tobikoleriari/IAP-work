7b1c4dd8cfaa3c7d8bbde76144f38caf7a90c051
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-19 08:03:19 +0300

    form handler

--- signupHandler.php
+++ signupHandler.php
@@ -1,3 +1,10 @@
 <?php
 require_once 'db.php';
 $conn=new PDO('mysql:host=localhost;dbname=iap-work','root','');
+
+if($_SERVER['REQUEST METHOD']=='POST'){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+}
