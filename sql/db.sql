f69faaf65ec8d9559469ccdd3d005aaae0f930f8
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-19 09:43:54 +0300

    corrected sql

--- sql/db.sql
+++ sql/db.sql
@@ -3,53 +3,15 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-
+-- Database: `iap-work`
 --
--- Database: `iap`
---
-DROP DATABASE IF EXISTS `iap`;
-CREATE DATABASE IF NOT EXISTS `iap` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `iap`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gender`
---
-
-DROP TABLE IF EXISTS `gender`;
-CREATE TABLE IF NOT EXISTS `gender` (
-  `genderId` tinyint(1) NOT NULL AUTO_INCREMENT,
-  `gender` varchar(20) NOT NULL DEFAULT '',
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`genderId`),
-  UNIQUE KEY `gender` (`gender`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `roleId` tinyint(1) NOT NULL AUTO_INCREMENT,
-  `role` varchar(20) NOT NULL DEFAULT '',
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`roleId`),
-  UNIQUE KEY `role` (`role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+DROP DATABASE IF EXISTS `iap-work`;
+CREATE DATABASE IF NOT EXISTS `iap-work` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `iap-work`;
 
 --
 -- Table structure for table `users`
 --
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userId` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -58,29 +20,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(60) NOT NULL DEFAULT '',
   `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `genderId` tinyint(1) NOT NULL DEFAULT 0,
-  `roleId` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`userId`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
-  KEY `genderId` (`genderId`),
-  KEY `roleId` (`roleId`)
+ 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `gender`
---
-ALTER TABLE `gender`
-  ADD CONSTRAINT `gender_ibfk_1` FOREIGN KEY (`genderId`) REFERENCES `users` (`genderId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `roles`
---
-ALTER TABLE `roles`
-  ADD CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `users` (`roleId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
