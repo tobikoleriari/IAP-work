b953281c18ea7e696a88da7c494311fc08e98d3e
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-19 08:18:22 +0300

    Inclusion of Bootstrap links

--- displayUsers.php
+++ displayUsers.php
@@ -5,3 +5,15 @@ $conn=new PDO('mysql:host=localhosst;dbname=iap-work','root','');
 $user =new phpHandler($conn);
 $users=$user->fetchData();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
