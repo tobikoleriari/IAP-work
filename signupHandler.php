63828677fa8a2d93f462e0134aebd9b02afeaba7
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-19 09:57:10 +0300

    corrected php

--- signupHandler.php
+++ signupHandler.php
@@ -3,7 +3,7 @@ require_once 'db.php';
 require_once 'phpHandler.php';
 $conn=new PDO('mysql:host=localhost;dbname=iap-work','root','');
 
-if($_SERVER['REQUEST METHOD']=='POST'){
+if($_SERVER['REQUEST_METHOD']=='POST'){
     $name=$_POST['name'];
     $email=$_POST['email'];
     $username=$_POST['username'];
