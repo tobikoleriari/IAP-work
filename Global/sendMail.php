a77506d39d8040b179760f8372f24c83489de9bc
tobikoleriari <peter.leriari@strathmore.edu>
2024-10-03 13:31:07 +0300

    created session

--- Global/sendMail.php
+++ Global/sendMail.php
@@ -1,4 +1,11 @@
 <?php
+// Start the session
+session_start(); // Start the session
+require 'C:/xampp/htdocs/PHPMailer/src/PHPMailer.php';
+require 'C:/xampp/htdocs/PHPMailer/src/SMTP.php';
+require 'C:/xampp/htdocs/PHPMailer/src/Exception.php';
+require 'C:/xampp/htdocs/PHPMailer/src/OAuth.php';
+
 
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
@@ -8,8 +15,23 @@ use PHPMailer\PHPMailer\Exception;
 class SendMail{
 
 //These must be at the top of your script, not inside a function
-public function SendMail($mail, $mailMsg){
+public function SendMail(){
+    if (isset($_POST['signup'])) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Capture form data
+            $username = trim($_POST['username']);
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
+            $confirm_password = trim($_POST['confirm_password']);
 
+
+            $otp = rand(100000, 999999);
+            $_SESSION['otp'] = $otp;
+            $_SESSION['user_data'] = [
+                'username' => $username,
+                'email' => $email,
+                'password' => $password
+            ];
     //Load Composer's autoloader
     require 'plugins/PHPMailer/vendor/autoload.php';
     
@@ -43,3 +65,6 @@ public function SendMail($mail, $mailMsg){
     }
 }
 }
+}
+}
+?>
