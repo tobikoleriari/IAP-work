03eed6bc1d9d4bfa55dbb6471ade169472ee88bb
tobikoleriari <peter.leriari@strathmore.edu>
2024-10-05 18:02:00 +0300

    removed page

---
+++ Users.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'phpHandler.php'; // Include the database handler
+
+class Users {
+    private $dbHandler;
+
+    public function __construct() {
+       
+        $conn = new PDO('mysql:host=localhost;dbname=iap-work', 'root', ''); 
+        $this->dbHandler = new phpHandler($conn);
+    }
+
+    public function createUser( $fullname, $username, $email, $password) {
+        // Create the user by inserting the data using phpHandler
+        return $this->dbHandler->insertData($fullname, $email, $username, $password);
+    }
+}
+?>
+
