7835c55c8d36dba4f9688e0a24a9007f3c0c0767
tobikoleriari <peter.leriari@strathmore.edu>
2024-10-05 14:33:07 +0300

    corrected phpmailer

--- sendMail.php
+++ sendMail.php
@@ -1,47 +1,70 @@
 <?php
+// Start the session
+session_start(); // Start the session
+require 'C:\xampp\htdocs\PHPMailer\src\PHPMailer.php';
+require 'C:\xampp\htdocs\PHPMailer\src\SMTP.php';
+require 'C:\xampp\htdocs\PHPMailer\src\Exception.php';
+// require 'C:/xampp/htdocs/PHPMailer/src/OAuth.php';
+
+
 //Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+class SendMail{
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+//These must be at the top of your script, not inside a function
+public function SendMail(){
+    if (isset($_POST['signup'])) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Capture form data
+            $username = trim($_POST['username']);
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
+            $confirm_password = trim($_POST['confirm_password']);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'tobikoleriari69@gmail.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('tobikoleriari69@gmail.com', 'Tobiko');
-    $mail->addAddress('peter.leriari@strathmore.edu', 'TOB LER');     //Add a recipient
-    //$mail->addAddress('ellen@example.com');               //Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+            $otp = rand(100000, 999999);
+            $_SESSION['otp'] = $otp;
+            $_SESSION['user_data'] = [
+                'username' => $username,
+                'email' => $email,
+                'password' => $password
+            ];
+    //Load Composer's autoloader
+    // require 'plugins/PHPMailer/vendor/autoload.php';
+    
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'ICS CLASS WORK';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'tobikoleriari69@gmail.com';                     //SMTP username
+        $mail->Password   = 'secret';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set 
+    
+        //Recipients
+        $mail->setFrom('tobikoleriari69@gmail.com', 'Test ICS');
+        $mail->addAddress($email);     //Add a recipient
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'TOBIKO OTP';
+        $mail->Body    = 'Your OTP is: ' . $otp;
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+    }
 }
+}
+}
+}
+?>
